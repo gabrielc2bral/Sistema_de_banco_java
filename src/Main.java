e9ced5741d9469ad710179228b8f740742a48e12
Gabriel Cabral <gabrielcabralbastos@gmail.com>
2025-06-09 22:13:49 -0300

    Adiciona funcionalidade de transferência entre contas

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
         Sacar sacar = new Sacar();
         Depositar depositar = new Depositar();
         int opcao;
+        double valor;
+        String numeroContaOrigem, numeroContaDestino, nome, cpf, numeroConta;
 
         do {
             System.out.println("\n==== MENU BANCO ====");
@@ -20,7 +22,8 @@ public class Main {
             System.out.println("2 - Depositar");
             System.out.println("3 - Sacar");
             System.out.println("4 - Exibir Saldo");
-            System.out.println("5 - Sair");
+            System.out.println("5 - Transferencia");
+            System.out.println("6 - Sair");
             System.out.print("Escolha uma opção: ");
 
             opcao = sc.nextInt();
@@ -28,18 +31,18 @@ public class Main {
             switch (opcao){
                 case 1:
                     System.out.println("Digite o nome da pessoa");
-                    String nome = sc.nextLine();
+                    nome = sc.nextLine();
                     System.out.println("Digite o cpf da pessoa");
-                    String cpf = sc.nextLine();
+                    cpf = sc.nextLine();
                     System.out.println("Digite o numero da conta");
-                    String numeroDaConta = sc.nextLine();
-                    Conta contaNova = new Conta(nome, cpf, numeroDaConta);
+                    numeroConta = sc.nextLine();
+                    Conta contaNova = new Conta(nome, cpf, numeroConta);
                     banco.addConta(contaNova);
                     break;
                 case 2:
                     System.out.println("Digite o numero da conta");
-                    String numerocontaDeposito = sc.nextLine();
-                    Conta contaDeposito = banco.buscarConta(numerocontaDeposito);
+                    numeroConta = sc.nextLine();
+                    Conta contaDeposito = banco.buscarConta(numeroConta);
                     if (contaDeposito != null){
                         System.out.println("Digite a quantia do deposito");
                         depositar.executar(sc.nextDouble(), contaDeposito);
@@ -50,32 +53,63 @@ public class Main {
                     break;
                 case 3:
                     System.out.println("Digite o numero da conta");
-                    String contaSaque = sc.nextLine();
-                    Conta contaSaq = banco.buscarConta(contaSaque);
-                    if (contaSaq != null){
+                    numeroConta = sc.nextLine();
+                    Conta contaSaque = banco.buscarConta(numeroConta);
+                    if (contaSaque != null){
                         System.out.println("Digite a quantia do saque");
-                        sacar.executar(sc.nextDouble(),contaSaq);
-                        System.out.printf("%nSaldo da conta %.2f", contaSaq.getSaldo());
+                        sacar.executar(sc.nextDouble(),contaSaque);
+                        System.out.printf("%nSaldo da conta %.2f", contaSaque.getSaldo());
                     }else {
                         System.out.println("Sua conta não existe");
                     }
                     break;
                 case 4:
                     System.out.println("Digite o numero da conta");
-                    String contaSaldo = sc.nextLine();
-                    Conta contaSald = banco.buscarConta(contaSaldo);
-                    if (contaSald != null){
-                        System.out.printf("%nSaldo da conta %.2f", contaSald.getSaldo());
+                    numeroConta = sc.nextLine();
+                    Conta contaSaldo = banco.buscarConta(numeroConta);
+                    if (contaSaldo != null){
+                        System.out.printf("%nSaldo da conta %.2f", contaSaldo.getSaldo());
                     }else {
                         System.out.println("Sua conta não existe");
                     }
                     break;
+                case 5:
+                    System.out.println("Digite o numero da sua conta");
+                    numeroContaOrigem = sc.nextLine();
+                    Conta conta = banco.buscarConta(numeroContaOrigem);
 
+                    if (conta != null){
+
+                        System.out.println("Digite o numero da conta que você deseja depositar: ");
+                        numeroContaDestino = sc.nextLine();
+                        Conta contaDestino  = banco.buscarConta(numeroContaDestino);
+
+                        if (contaDestino != null){
+
+                            System.out.printf("Digite o valor que deseja transferir para %s: ", contaDestino.getNome());
+                            valor = sc.nextDouble();
+                            sc.nextLine();
+                            if(valor > 0 && valor <= conta.getSaldo()){
+                                conta.setSaldo(conta.getSaldo() - valor);
+                                contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+                                System.out.println("Sua transferência foi realizada com sucesso");
+                                System.out.printf("%nSaldo da sua conta %.2f", contaDestino.getSaldo());
+                            }else {
+                                System.out.println("Sua conta não tem saldo suficiente");
+                        }
+                        }else {
+                            System.out.println("Essa conta não existe");
+                        }
+                    }else {
+                        System.out.println("Sua conta não existe");
+                    }
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha uma opção de 1 a 6.");
+                    break;
             }
 
-
-
-        }while (opcao !=5);
+        }while (opcao !=6);
 
         sc.close();
     }
